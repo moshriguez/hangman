813f0259606310f1544f47f349551f53b9f7bbd4
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-11-15 22:38:12 -0800

    basic components built

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Header from './components/Header'
+import Figure from './components/Figure'
+import WrongLetters from './components/WrongLetters'
+import Word from './components/Word'
 
 function App() {
   return (
     <>
       <Header/>
+      <div className="game-container">
+        <Figure/>
+        <WrongLetters/>
+        <Word/>
+      </div>
     </>
   );
 }
