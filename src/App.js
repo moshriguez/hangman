702b1b371730e7502c172dd88085bd47934b3816
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-11-15 23:19:46 -0800

    add notification for dupe letters

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Header from './components/Header'
 import Figure from './components/Figure'
 import WrongLetters from './components/WrongLetters'
 import Word from './components/Word'
+import Popup from './components/Popup'
+import Notification from './components/Notification'
+import {showNotification as show} from './helpers/helpers'
 
 const words = ['application', 'programming', 'interface', 'wizard'];
 let selectedWord = words[Math.floor(Math.random() * words.length)];
@@ -13,6 +16,7 @@ function App() {
   const [playable, setPlayable] = useState(true)
   const [correctLetters, setCorrectLetters] = useState([])
   const [wrongLetters, setWrongLetters] = useState([])
+  const [showNotification, setShowNotification] = useState(false)
 
   useEffect(() => {
     const handleKeydown = event => {
@@ -24,13 +28,13 @@ function App() {
           if (!correctLetters.includes(letter)) {
             setCorrectLetters(currentLetters => [...currentLetters, letter])
             } else {
-            // showNotification();
+            show(setShowNotification)
           }
         } else {
           if (!wrongLetters.includes(letter)) {
             setWrongLetters(wrongLetters => [...wrongLetters, letter])  
           } else {
-            // showNotification();
+            show(setShowNotification)
           }
         }
       }
@@ -48,6 +52,8 @@ function App() {
         <WrongLetters wrongLetters={wrongLetters} />
         <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
+        <Popup />
+        <Notification showNotification={showNotification} />
     </>
   );
 }
