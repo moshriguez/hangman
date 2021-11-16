91aaf43ca2f3e2390095a1e5a946f656712b754d
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-11-15 23:09:08 -0800

    add event listener and logic for figure, word and wrong letters

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
+import { useEffect, useState } from 'react'
+
 import './App.css';
 import Header from './components/Header'
 import Figure from './components/Figure'
 import WrongLetters from './components/WrongLetters'
 import Word from './components/Word'
 
+const words = ['application', 'programming', 'interface', 'wizard'];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 function App() {
+  const [playable, setPlayable] = useState(true)
+  const [correctLetters, setCorrectLetters] = useState([])
+  const [wrongLetters, setWrongLetters] = useState([])
+
+  useEffect(() => {
+    const handleKeydown = event => {
+      const { key, keyCode } = event
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+  
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters(currentLetters => [...currentLetters, letter])
+            } else {
+            // showNotification();
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters(wrongLetters => [...wrongLetters, letter])  
+          } else {
+            // showNotification();
+          }
+        }
+      }
+    }
+    window.addEventListener('keydown', handleKeydown)
+
+    return () => window.removeEventListener('keydown', handleKeydown)
+  }, [correctLetters, wrongLetters, playable])
+
   return (
     <>
-      <Header/>
+      <Header />
       <div className="game-container">
-        <Figure/>
-        <WrongLetters/>
-        <Word/>
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
     </>
   );
